cdfa8560133058cc76a76fb845f6a9fd540fcda3
Nikita <vclan@mail.ru>
2021-09-17 19:16:45 +0300

    adding dist subtree

--- src/components/FileView.vue
+++ src/components/FileView.vue
@@ -1,18 +1,44 @@
 <template>
-  <p class="file">{{name}}</p>
+	<p class="file">
+		<img
+			src="https://cdn2.iconfinder.com/data/icons/kids/128x128/actions/editcopy.png"
+		/>
+		{{ name }}
+	</p>
 </template>
 
 <script>
 export default {
-  name: 'FileView',
-  props: {
-    name: String
-  }
-}
+	name: 'FileView',
+	props: {
+		name: String,
+	},
+};
 </script>
 
 <style scoped lang="scss">
-.file{
-  color: green;
+.file {
+	color: green;
+	width: 200px;
+	height: 40px;
+	background: #3e4363;
+	border: 1px solid #ce7077;
+	border-radius: 10px;
+	box-shadow: 5px 5px 10px #3e4363;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	font-weight: bold;
+	cursor: pointer;
+	transition: transform 0.3s ease-out;
+	img {
+		height: 30px;
+	}
+	&:hover {
+		transform: scale(1.2);
+	}
+	&:active {
+		color: white;
+	}
 }
 </style>
