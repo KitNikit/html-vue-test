04de691673c6caac68e66c508f669bd4de6a2af2
Nikita <vclan@mail.ru>
2021-09-17 19:06:24 +0300

    adding dist subtree

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <FolderView name="$" :folders="folders" :files="files" />
+	<FolderView name="$" :folders="folders" :files="files" />
 </template>
 
 <script>
-import FolderView from './components/FolderView.vue'
+import FolderView from './components/FolderView.vue';
 import data from './data/list';
 
 export default {
-  name: 'App',
-  components: {
-    FolderView
-  },
-  data: () => data
-}
+	name: 'App',
+	components: {
+		FolderView,
+	},
+	data: () => data,
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	padding: 20px;
+	background: url(https://vsthemes.org/uploads/posts/2020-08/1598709615_screenshot_1.jpg)
+		center center / cover no-repeat;
 }
 </style>
